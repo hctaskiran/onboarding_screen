c0a21aaf66550a7777584160d74e013ff00912c5
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2024-03-10 22:20:28 +0300

    Initial Commit

---
+++ onboarding_screen/onboarding_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  onboarding_screenApp.swift
+//  onboarding_screen
+//
+//  Created by Haktan Can Taşkıran on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct onboarding_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
